81665b7fb93d127dd73475cbf6439c069e6879e0
Aleksejs Cetverikovs <kerd1k@gmail.com>
2024-10-29 09:33:26 +0200

    typescript settings

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-import { DocumentGenerator } from "./DocumentGenerator";
+#!/usr/bin/env node
+
+// import { DocumentGenerator } from "./DocumentGenerator";
 
 //TODO: parse args + default params
-const sourcePath = "";
-const apiEndpoint = "";
-const savePath = "";
-const savePathFileName = "";
+// const sourcePath = "";
+// const apiEndpoint = "";
+// const savePath = "";
+// const savePathFileName = "";
 
-new DocumentGenerator({
-    sourcePath,
-    apiEndpoint,
-    savePath,
-    savePathFileName,
-});
+// new DocumentGenerator({
+//     sourcePath,
+//     apiEndpoint,
+//     savePath,
+//     savePathFileName,
+// });
+
+// const args = process.argv;
+const [, , ...args] = process.argv;
+
+console.log(`Welcome to The Matrix, Neo. ${args}`);
